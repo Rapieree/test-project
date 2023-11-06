9ff626b844883b90115d05952618680511c2c37b
Сергей <serega.mironov11@gmail.com>
2023-11-07 02:25:28 +0300

    Change file extension for pages and apis modules

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
     dirs: [`src`], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
     ignoreDuringBuilds: true,
   },
+  pageExtensions: [`page.jsx`, `api.js`, `page.tsx`, `api.ts`],
 };
 
 module.exports = nextConfig;
